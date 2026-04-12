8b1b57527afbc101d4b24cc86bb0fce64e50eef1
yakash-goyal <yakashgoyal123@gmail.com>
2026-04-12 23:24:26 +0530

    feat: Update package.json and package-lock.json with new dependencies and fix import path in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
-import connectDB from './config/db'
+import connectDB from './config/db.js'
 import cors from 'cors'
 
 let app = express()
@@ -10,6 +10,10 @@ app.use(express.json())
 app.use(cors({
     origin: 'http://localhost:5173'
 }))
+
+app.get("/",(req,res)=>{
+    res.json({message :"Hello my name is yash"})
+})
 
 connectDB().then(()=>{
     app.listen(process.env.PORT,()=>{
